ae23005303e2d75fc19ba96eca334d7d8d3bb955
chapsaldduck <koala031532@gmail.com>
2023-02-07 23:51:54 +0900

    modified: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ import "./style/App.scss";
 
 function App() {
   const [isClicked, setIsClicked] = useState(0);
+  const [isLat, setIsLat] = useState();
+  const [isLon, setIsLon] = useState();
   const markets = data;
-  // console.log(isClicked);
+
+  setInterval(() => getLocate(), 1000);
+
+  const getLocate = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setIsLat(position.coords.latitude);
+        setIsLon(position.coords.longitude);
+      });
+    }
+  };
+  const getMyMarkers = () => {
+    console.log("Me!");
+    return (
+      <MapMarker
+        key={"myLocation"}
+        position={{ lat: isLat, lng: isLon }}
+        clickable={true}
+      />
+    );
+  };
+
   const getMarkers = (data) => {
     const result = [];
     for (let i = 0; i < Object.keys(data).length; i++) {
@@ -22,13 +45,45 @@ function App() {
     return result;
   };
 
+  const getOpenTime = () => {
+    if (isClicked !== 0) {
+      return (
+        <div id="opentime">
+          <span className="title">영업시간</span>
+          <br />
+          <br />
+          <span>{`월: ${data[isClicked].mon}` ?? " "}</span>
+          <br />
+          <span>화: {data[isClicked].tue ?? " "}</span>
+          <br />
+          <span>수: {data[isClicked].wed ?? " "}</span>
+          <br />
+          <span>목: {data[isClicked].thu ?? " "}</span>
+          <br />
+          <span>금: {data[isClicked].fri ?? " "}</span>
+          <br />
+          <span>토: {data[isClicked].sat ?? " "}</span>
+          <br />
+          <span>일: {data[isClicked].sun ?? " "}</span>
+        </div>
+      );
+    }
+    return " ";
+  };
+
   return (
     <Map
-      center={{ lat: 37.5579, lng: 126.9244 }} // 홍대입구역
-      style={{ width: "100%", height: "100vh", zIndex: "10" }} // 화면 전체
+      center={{
+        lat: isLat ?? 37.5579,
+        lng: isLon ?? 126.9244,
+      }} // 현재 위치 시작, default 홍대입구역
+      id="Map"
     >
       {getMarkers(markets)}
+      {getMyMarkers()}
       <div id="info">
+        <img src="./assets/logo/모쿠.png" alt="" />
+        <span className="line"></span>
         <div className="text">
           <div id="title">{data[isClicked].name ?? "이름 정보 없음"}</div>
           <br />
@@ -37,6 +92,10 @@ function App() {
           <div id="address">{data[isClicked].address ?? "주소 정보 없음"}</div>
           <br />
           <div id="number">{data[isClicked].number ?? "전화번호 없음"}</div>
+          <br />
+
+          {/* 영업시간 */}
+          {getOpenTime()}
         </div>
       </div>
     </Map>
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import create from "zustand";
-
-const useStore = create((set) => ({
-  isOpen: false,
-  //   setIsOpen: () => set((state) => ({ isOpen: true })),
-  setIsOpen: false,
-}));
-export default useStore;
