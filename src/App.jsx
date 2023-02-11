5313a91f03a89f106c8ca18a052ee3139bff5dae
chapsaldduk <koala031532@gmail.com>
2023-02-11 16:18:17 +0900

    modified: start location function

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 import data from "./assets/markets.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./style/App.scss";
 
 function App() {
   const [isClicked, setIsClicked] = useState(0);
-  // const [isChanged, setIsChanged] = useState(false);
   const [isLat, setIsLat] = useState();
   const [isLon, setIsLon] = useState();
   const [isStartLat, setIsStartLat] = useState();
   const [isStartLon, setIsStartLon] = useState();
   const markets = data;
 
-
-  const getStartLocate = () =>{
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        setIsStartLat(position.coords.latitude);
-        setIsStartLon(position.coords.longitude);
-      });
-    }
-    else{
-      // default: 홍대입구역
-      setIsLat(37.5579);
-      setIsLon(126.9244);
+useEffect(()=>{
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setIsStartLat(position.coords.latitude);
+      setIsStartLon(position.coords.longitude);
+    });
   }
+  else{
+    // default: 홍대입구역
+    setIsStartLat(37.5579);
+    setIsStartLon(126.9244);
 }
+}, [])
+
 
   /** 현재 위치 정보 -> state isLat, isLon에 저장 */
   const getLocate = () => {
@@ -120,9 +119,6 @@ function App() {
   //홍대입구역: {lat: 37.5579, lng: 126.9244}
   return (
     <div id="App">
-      {
-      getStartLocate() //시작위치 가져오기
-      }
       <Map
         center={{
           lat: isStartLat ?? 37.5579,
